40a3917895e463e63f90c7a05cac35b1f8aff970
Andrew54757 <andrews54757@gmail.com>
2016-09-09 14:18:22 -0400

    Update store.js

--- store.js
+++ store.js
@@ -4,6 +4,30 @@ module.exports = class store {
   constructor(defaultr) {
     this.repos = [];
     this.default = defaultr
+    this.list = [];
+    this.compile();
+  }
+  checkifvalid(a) {
+    return true
+  }
+  compile() {
+    request(this.default + "/plugins.json",function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if (!this.checkifvalid(body)) return;
+          var a = JSON.parse(body);
+          this.list.concat(a);
+        }
+    });
+    this.repos.forEach((repo)=>{
+       request(repo + "/plugins.json",function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if (!this.checkifvalid(body)) return;
+          var a = JSON.parse(body);
+          this.list.concat(a);
+        }
+    });
+    });
+    
   }
   search() {
     
