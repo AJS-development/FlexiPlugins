9db7fb630b4df5fd254b2a6653105b2b8d21e9ab
Andrew54757 <andrews54757@gmail.com>
2016-09-09 14:11:37 -0400

    Update and rename functions.js to store.js

---
+++ store.js
@@ -0,0 +1,13 @@
+"use strict";
+var request = require('request');
+module.exports = class store {
+  constructor(defaultr) {
+    this.repos = [];
+    this.default = defaultr
+  }
+  search() {
+    
+    
+  }
+  
+}
