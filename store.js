ad42d81977a1a8beec9d44ec59d36d8c996f4bb9
Andrew54757 <andrews54757@gmail.com>
2016-09-09 14:35:38 -0400

    Update store.js

--- store.js
+++ store.js
@@ -29,8 +29,49 @@ module.exports = class store {
     });
     
   }
-  search() {
-    
+  search(a) {
+    var title = [];
+    var desc = [];
+    var others = []
+    this.list.forEach((item)=>{
+      if (item.name) {
+        var b = item.name.toLowerCase();
+        if (b.indexOf(a.toLowerCase()) != -1) {
+          title.push(item)
+          return;
+        }
+      }
+      if (item.description) {
+        if (item.description.toLowerCase().indexOf(a.toLowerCase()) != -1) {
+         desc.push(item) 
+        return;
+        }
+      }
+      var b = a.split(" ")
+      var f = 0;
+      b.every((c)=>{
+        if (f > 5) {
+          others.push(item)
+        return false;
+          
+        }
+        if (item.title && item.title.toLowerCase().indexOf(c.toLowerCase()) != -1) {
+          f += 2;
+        return true;
+        }
+        if (item.description && item.description.toLowerCase().indexOf(c.toLowerCase()) != -1) {
+         f ++;
+         return true;
+        }
+      })
+      
+      
+    })
+    var final = [];
+    final.concat(title);
+    final.concat(desc);
+    final.concat(others);
+    return final
     
   }
   
