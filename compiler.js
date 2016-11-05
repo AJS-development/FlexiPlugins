35f15e48e64952c29ec0da1606a98f7c208e138f
Andrew S <andrews54757@gmail.com>
2016-11-04 21:34:26 -0400

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,100 +1,97 @@
 var fs = require('fs')
-
-module.exports = function(folder){
-  var final;
+module.exports = {
+compile: function(folder){
+  var final = "";
     var len = 0;
     var toWrite = [];
+  
+   var glob = false;
 var walk= function(path) {
-  if( fs.existsSync(path) ) {
-    fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
+    if (!glob) {
+      glob = path
+      path = ""
+    } 
+  if( fs.existsSync(glob + path) ) {
+    fs.readdirSync(glob + path).forEach(function(file,index){
+      var curPath = glob + path + "/" + file;
+        var sdir = path + "/" + file
       if(fs.lstatSync(curPath).isDirectory()) { // recurse
-        path(curPath);
+        walk(sdir);
       } else {
-        parseFile(curPath);
+          
+        parseFile(curPath,sdir);
       }
     });
     
   }
 };
-  
-  function parseFile(dir) {
-      var buf = fs.readFileSync(dir,'utf8')
-      len += buf.length*2 + 2 + dir.length*2
-      
-    toWrite.push({b:buf,f:dir})
-  }
-    var offset = 0;
-  final = new ArrayBuffer(len + 1)
-  var view= new DataView(buf)
-  for (var i in toWrite) {
-      
-      var write = toWrite[i]
-       view.setUint8(0, 0);
-      offset ++;
-      for (var j =0;j< write.f.length;j++) {
-          var char = write.f.charCodeAt(j);
-          view.setUint8(offset, char);
-          offset ++;
-      }
-      view.setUint8(0, 0);
-      offset ++;
-      for (var j =0;j< write.b.length;j++) {
-          var char = write.b.charCodeAt(j);
-          view.setUint8(offset, char);
-          offset ++;
-      }
-     
-      
-  }
-    view.setUint8(offset,1)
-    return String.fromCharCode.apply(null, new Uint16Array(final));
-    
-}
-
-module.exports = function(str) {
-     var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i=0, strLen=str.length; i<strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-    var view = new DataView(buf)
-    
-    var offset = 0;
-    var char;
-    var data = [];
-    var a = "";
-    var current = false
-  
-    var toggle = false
-while (1==1) {
-    char = view.getUint8(offset)
-    offset ++;
-    
-    
-    if (char ==  0) {
-        if (toggle) {
-            if (current) current.d = a
-            
-        
-        } else {
-       if (current) current.b = a
-     current = {
-        d: '',
-        b: ''
-    }
+function append(num) {
    
-     data.push(current)
-        }
-          a = ""
-        toggle = !toggle
-        continue;
-    }
-    if (char == 1) {
-        if (current) current.b = a
-        break;
-    }
-      a += String.fromCharCode(char)
+    final += num.toString() + " "
+    
 }
-    return data
+  function parseFile(dir,sdir) {
+    //  console.log(dir)
+      var file = fs.readFileSync(dir,'utf8')
+      if (!file || !dir || !sdir) return
+  append(0)   
+  
+ for (var i = 0; i < sdir.length; i ++) {
+    
+     
+     append(sdir.charCodeAt(i))
+ }
+      append(0)   
+      for (var i = 0; i < file.length; i ++) {
+     append(file.charCodeAt(i))
+ }
+  }
+    walk(folder);
+    
+   return final
+},
+decompile: function(text,savedir) {
+    text = text.split(" ")
+    var toggle = false;
+    var a = [];
+
+var dir = ""
+    for (var i = 0; i < text.length; i ++) {
+        var char = parseInt(text[i])
+        if (char == 0) {
+            if (toggle) {
+                
+                dir = a.join("")
+                a = [];
+            } else {
+            if (dir.length > 0) {
+                var d = dir.split("/")
+                var p = d.slice(0,d.length - 1).join("/")
+                try {
+                    
+                    
+                    fs.statSync(savedir + p)
+                    
+                } catch (e) {
+                    console.log(e)
+                    if (p) fs.mkdirSync(savedir + p)
+                
+                }
+         
+               if (dir) fs.writeFileSync(savedir + dir,a.join(""))
+            }
+                a = [];
+                }
+            toggle = !toggle
+            continue;
+        }
+        a.push(String.fromCharCode(char))
+    }
+    if (a.length > 0) {
+               if (dir) fs.writeFileSync(savedir + dir,a.join(""))
+            }
+    a = [];
+    
+    
+}
 }
